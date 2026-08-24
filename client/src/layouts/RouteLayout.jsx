e23478a7bb9f6a0bbc77aa6e58945ce75bb4363c
mmanohar24 <manojcareers24@gmail.com>
2026-08-24 22:10:14 +0530

    Style navbar

--- client/src/layouts/RouteLayout.jsx
+++ client/src/layouts/RouteLayout.jsx
@@ -4,18 +4,28 @@ const RouteLayout = () => {
 
     return (
         <div>
-            <header>
-                <nav>
-                    <NavLink to="/"> Home </NavLink>
-                    <NavLink to="/login"> Login </NavLink>
-                    <NavLink to="/signup"> Sign Up </NavLink>
-                    <NavLink to="/buy"> Buy </NavLink>
-                </nav>
+
+            <header className="header px-8 py-4 bg-purple-700 rounded-lg mx-6 mt-4">
+                <div className="nav flex justify-between items-center">
+
+                    <div>
+                        <p className="text-white font-bold text-xl italic">  Motor Archive </p>
+                    </div>
+
+                    <nav className="flex gap-6">
+                        <NavLink to="/" className="text-white hover:text-purple-200 transition-colors"> Home </NavLink>
+                        <NavLink to="/login" className="text-white hover:text-purple-200 transition-colors"> Login </NavLink>
+                        <NavLink to="/signup" className="text-white hover:text-purple-200 transition-colors"> Sign Up </NavLink>
+                        <NavLink to="/buy" className="text-white hover:text-purple-200 transition-colors"> Buy </NavLink>
+                    </nav>
+
+                </div>
             </header>
 
             <main>
                 <Outlet />
             </main>
+
         </div>
     )
 
